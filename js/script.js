0ca98dbd795ad50bcdd70c7cc546c8ec371f238c
Jhorsh-code <jhorschclintonq@gmail.com>
2026-02-23 23:03:49 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log("Retro Movie cargado correctamente");
+document.querySelectorAll("nav a").forEach(link => {
+    link.addEventListener("mouseover", () => {
+        link.style.transform = "scale(1.1)";
+    });
+
+    link.addEventListener("mouseout", () => {
+        link.style.transform = "scale(1)";
+    });
+});
